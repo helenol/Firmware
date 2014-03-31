37641263b1147576f32b42406e0b37e271122d6c
Helen Oleynikova <helenoleynikova@gmail.com>
2014-03-31 13:12:28 +0200

    Added helen pos control.

---
+++ src/modules/helen_pos_control/helen_pos_control_params.c
@@ -0,0 +1,54 @@
+#include "helen_pos_control_params.h"
+
+PARAM_DEFINE_FLOAT(HPC_M_KG, 0.478f);
+PARAM_DEFINE_FLOAT(HPC_THRUST_SCALE, 7.0f);
+PARAM_DEFINE_FLOAT(HPC_KP_Z, 1.0f);
+PARAM_DEFINE_FLOAT(HPC_KD_Z, 0.0f);
+PARAM_DEFINE_FLOAT(HPC_KP_XY, 1.0f);
+PARAM_DEFINE_FLOAT(HPC_KD_XY, 0.0f);
+PARAM_DEFINE_FLOAT(HPC_F_LIM_Z, 10.0f);
+PARAM_DEFINE_FLOAT(HPC_F_LIM_XY, 4.0f);
+
+/*
+int parameters_init(struct position_estimator_flow_param_handles *h)
+{
+    h->sigma_acc = param_find("FLOW_S_ACC");
+    h->sigma_acc_bias = param_find("FLOW_S_ACC_BIAS");
+    h->sigma_baro = param_find("FLOW_S_BARO");
+    h->sigma_baro_bias = param_find("FLOW_S_BARO_BIAS");
+    h->sigma_flow = param_find("FLOW_S_FLOW");
+    h->sigma_sonar = param_find("FLOW_S_SNR");
+    h->sigma_pos_noise = param_find("FLOW_S_POS_NOISE");
+    h->sigma_vel_noise = param_find("FLOW_S_VEL_NOISE");
+    h->sigma_acc_noise = param_find("FLOW_S_ACC_NOISE");
+    h->sonar_min_value = param_find("FLOW_SNR_MIN_VAL");
+    h->sonar_max_value = param_find("FLOW_SNR_MAX_VAL");
+    h->sonar_mean_threshold = param_find("FLOW_SNR_MEAN_THR");
+    h->sonar_vel_threshold = param_find("FLOW_SNR_VEL_THR");
+    h->flow_frame_rate = param_find("FLOW_FLOW_FRAME_RATE");
+
+    return OK;
+}
+
+
+int parameters_update(const struct position_estimator_flow_param_handles *h,
+                      struct position_estimator_flow_params *p)
+{
+    param_get(h->sigma_acc, &(p->sigma_acc));
+    param_get(h->sigma_acc_bias, &(p->sigma_acc_bias));
+    param_get(h->sigma_baro, &(p->sigma_baro));
+    param_get(h->sigma_baro_bias, &(p->sigma_baro_bias));
+    param_get(h->sigma_flow, &(p->sigma_flow));
+    param_get(h->sigma_sonar, &(p->sigma_sonar));
+    param_get(h->sigma_pos_noise, &(p->sigma_pos_noise));
+    param_get(h->sigma_vel_noise, &(p->sigma_vel_noise));
+    param_get(h->sigma_acc_noise, &(p->sigma_acc_noise));
+    param_get(h->sonar_min_value, &(p->sonar_min_value));
+    param_get(h->sonar_max_value, &(p->sonar_max_value));
+    param_get(h->sonar_mean_threshold, &(p->sonar_mean_threshold));
+    param_get(h->sonar_vel_threshold, &(p->sonar_vel_threshold));
+    param_get(h->flow_frame_rate, &(p->flow_frame_rate));
+
+    return OK;
+}
+*/
---
+++ src/modules/helen_pos_control/helen_pos_control_params.h
@@ -0,0 +1,65 @@
+#ifndef HELEN_POS_CONTROL_PARAMS_H
+#define HELEN_POS_CONTROL_PARAMS_H
+
+#include <systemlib/param/param.h>
+
+struct helen_pos_control_params {
+    float m;
+    float thrust_scale;
+    float k_p_gain_z;
+    float k_d_gain_z;
+    float k_p_gain_xy;
+    float k_d_gain_xy;
+    float f_lim_xy;
+    float f_lim_z;
+};
+
+struct helen_pos_control_param_handles {
+    param_t m;
+    param_t thrust_scale;
+    param_t k_p_gain_z;
+    param_t k_d_gain_z;
+    param_t k_p_gain_xy;
+    param_t k_d_gain_xy;
+    param_t f_lim_xy;
+    param_t f_lim_z;
+};
+
+/**
+ * Initialize all parameter handles and values
+ *
+ */
+int parameters_init(struct helen_pos_control_param_handles *h)
+{
+    h->m = param_find("HPC_M_KG");
+    h->thrust_scale = param_find("HPC_THRUST_SCALE");
+    h->k_p_gain_z = param_find("HPC_KP_Z");
+    h->k_d_gain_z = param_find("HPC_KD_Z");
+    h->k_p_gain_xy = param_find("HPC_KP_XY");
+    h->k_d_gain_xy = param_find("HPC_KD_XY");
+    h->f_lim_xy = param_find("HPC_F_LIM_XY");
+    h->f_lim_z = param_find("HPC_F_LIM_Z");
+
+    return OK;
+}
+
+/**
+ * Update all parameters
+ *
+ */
+int parameters_update(const struct helen_pos_control_param_handles *h,
+                      struct helen_pos_control_params *p)
+{
+    param_get(h->m, &(p->m));
+    param_get(h->thrust_scale, &(p->thrust_scale));
+    param_get(h->k_p_gain_z, &(p->k_p_gain_z));
+    param_get(h->k_d_gain_z, &(p->k_d_gain_z));
+    param_get(h->k_p_gain_xy, &(p->k_p_gain_xy));
+    param_get(h->k_d_gain_xy, &(p->k_d_gain_xy));
+    param_get(h->f_lim_xy, &(p->f_lim_xy));
+    param_get(h->f_lim_z, &(p->f_lim_z));
+
+    return OK;
+}
+
+#endif
