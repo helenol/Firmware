2563fabbbdc55e1677154e5673838a53fd573027
Helen Oleynikova <helenoleynikova@gmail.com>
2014-05-16 15:55:24 +0200

    Everything is horribly broken.

--- makefiles/config_px4fmu-v1_default.mk
+++ makefiles/config_px4fmu-v1_default.mk
@@ -78,10 +78,10 @@ MODULES     += modules/position_estimator_flow
 #
 # Vehicle Control
 #
-MODULES		+= modules/fw_pos_control_l1
-MODULES		+= modules/fw_att_control
+#MODULES		+= modules/fw_pos_control_l1
+#MODULES		+= modules/fw_att_control
 MODULES		+= modules/mc_att_control
-MODULES		+= modules/mc_pos_control
+#MODULES		+= modules/mc_pos_control
 MODULES     += modules/helen_pos_control
 #MODULES		+= examples/flow_position_control
 #MODULES		+= examples/flow_speed_control
